d12a5f8bf657850f6d000a8d012d484586b3cbc6
rtan1s <r0b3rt0xx@gmail.com>
2017-03-27 17:26:50 +0200

    RT: v0.02 - Added sample template, updated sh script, updated template and parameter file

--- AzureCSAVM/install_dckrCSA.sh
+++ AzureCSAVM/install_dckrCSA.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
+#
+# Execute APT repositories update and system upgrade
 sudo apt-get update
 sudo apt-get upgrade -y
-#install gnome desktop
-#sudo apt-get install ubuntu-desktop -y
-#install xrdp
+#install xrdp (used to connect to the VM using RDP)
 sudo apt-get install xrdp -y
-
-#using xfce if you are using Ubuntu version later than Ubuntu 12.04LTS
-sudo apt-get install xfce4 -y
-#sudo apt-get install xubuntu-desktop -y
+# Using xfce if you are using Ubuntu version later than Ubuntu 12.04LTS (...and this is the case...)
+sudo apt-get install xubuntu-desktop -y
+# Configure XFCE4
 sudo echo xfce4-session >/root/.xsession
 sudo sed -i '/\/etc\/X11\/Xsession/i xfce4-session' /etc/xrdp/startwm.sh
 sudo service xrdp restart
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+# Install or upgrade packages needed to install Docker
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+# Add Docker Repo to VM APT repos
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+#Install Docker
 sudo apt-get install docker-ce
+# Install correct version of docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.11.2/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+# Save current IP Address and Hostname in a variable
+vmipaddr=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+vmname=$(hostname)
 mkdir csa
 cd csa
-curl -k -L https://github.com/HewlettPackard/csa-ce/raw/master/buildEnv-dockercompose.sh | bash /dev/stdin <hostname> <ipaddress> [<proxyhost>] [<proxyport>]
+# Download HPE script to configure Docker Compose YAML file and execute it
+# since the VM is on Azure Proxy parameters are optional ( [<proxyhost>] [<proxyport>])
+curl -k -L https://github.com/HewlettPackard/csa-ce/raw/master/buildEnv-dockercompose.sh | bash /dev/stdin $vmname $vmipaddr
